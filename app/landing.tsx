649ef5b954f99df1865876333ab08c84b590e3de
Francis7575 <francis.albert.2009@gmail.com>
2025-03-24 19:49:18 -0700

    created landing page

---
+++ app/landing.tsx
@@ -0,0 +1,48 @@
+import { View, Text, Image } from "react-native";
+import React from "react";
+import Colors from "@/data/Colors";
+import Button from "@/components/shared/Button";
+import { useRouter } from "expo-router";
+
+export default function LandingScreen() {
+    const router = useRouter()
+
+  return (
+    <View>
+      <Image
+        source={require("../assets/images/login.png")}
+        style={{ width: "100%", height: 450 }}
+      />
+      <View style={{ padding: 20 }}>
+        <Text style={{ fontSize: 30, fontWeight: "bold", textAlign: "center" }}>
+          Welcome to College Campus Francis
+        </Text>
+        <Text
+          style={{
+            fontSize: 17,
+            textAlign: "center",
+            marginTop: 10,
+            color: Colors.GRAY,
+          }}
+        >
+          Your college news, updated in your pocket, Join the club, Registered
+          for new events and more!
+        </Text>
+        <Button
+          text="Get Started"
+          onPress={() => router.push("/(auth)/SignUp")}
+        />
+        <Text
+          style={{
+            fontSize: 16,
+            textAlign: "center",
+            color: Colors.GRAY,
+            marginTop: 7,
+          }}
+        >
+          Already have an account? Sign In Here
+        </Text>
+      </View>
+    </View>
+  );
+}
---
+++ data/Colors.ts
@@ -0,0 +1,5 @@
+export default {
+    GRAY: 'gray',
+    PRIMARY: '#2a9eBf',
+    WHITE: 'white'
+}
